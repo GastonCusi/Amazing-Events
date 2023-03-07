93a8c3358b0f73930365cfb0ea618482db7e54a8
Gastón <gastoncusi_90@hotmail.com>
2023-03-07 15:58:30 -0300

    Task2

---
+++ upcoming_events.js
@@ -0,0 +1,20 @@
+let cartaPrincipal = document.getElementById("carta-dinamica");
+console.log(cartaPrincipal);
+let stringHTML = "";
+
+for (events of data.events) {
+  if (events.date >= data.currentDate) {
+    console.table(events);
+
+    stringHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src="${events.events}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${events.name}</h5>
+            <p class="card-text">${events.description}</p>
+            <h4>${"Price $ " + events.price}</h4>
+            <a href="./details.html" class="btn btn-primary">Ver más</a>
+        </div>
+    </div>`;
+  }
+}
